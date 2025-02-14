44490ab6954c64c9b957bfa3e1a3bed4cf80bcc3
Sleuthing <anaslaykah@yahoo.com>
2025-02-14 10:11:20 +0100

    1st

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,10 +4,12 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Fprint
+// var _ = fmt.Fprint
+var builtin = []string{"exit", "echo"}
 
 func main() {
 	// Uncomment this block to pass the first stage
@@ -20,11 +22,18 @@ func main() {
 			os.Exit(0)
 		case "echo":
 			fmt.Println(command[5 : len(command)-1])
+		case "type":
+			if slices.Contains(builtin, command[5:len(command)-1]) {
+				fmt.Println(cmd + " is a shell builtin")
+			} else {
+				fmt.Println(cmd + ": not found")
+			}
 		default:
 			fmt.Println(command[:len(command)-1] + ": command not found")
 		}
 
 		if err != nil {
+			//panic(err)
 			fmt.Fprintln(os.Stderr, "Error reading input:", err)
 			os.Exit(1)
 		}
