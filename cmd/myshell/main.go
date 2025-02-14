46642a6d804159056e362d832d45069de5a8bfcb
Sleuthing <anaslaykah@yahoo.com>
2025-02-14 08:48:32 +0100

    2nd

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -13,6 +13,11 @@ func main() {
 	// Uncomment this block to pass the first stage
 	fmt.Fprint(os.Stdout, "$ ")
 
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Fprint(os.Stderr, " ")
+	}
+	fmt.Println(command[:len(command)-1] + ": command not found")
 	// Wait for user input
 	bufio.NewReader(os.Stdin).ReadString('\n')
 }
