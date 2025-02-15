70bc96d55d86d3c0a6b473b064713aada74302f5
Sleuthing <anaslaykah@yahoo.com>
2025-02-15 23:16:32 +0100

    type_command_extension_test_1

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"path/filepath"
 	"slices"
 	"strings"
 )
@@ -11,6 +12,7 @@ import (
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 // var _ = fmt.Fprint
 var builtin = []string{"exit", "echo", "type"}
+var PATH = os.Getenv("PATH")
 
 func process_command(command string) (string, string) {
 	if strings.Count(command, " ") >= 1 {
@@ -23,10 +25,10 @@ func process_command(command string) (string, string) {
 func main() {
 	for i := 0; i < 100; i++ {
 		fmt.Fprint(os.Stdout, "$ ")
-		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-		cmd, arg := process_command(command)
-
-		switch cmd {
+		full_command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		command_keyword, arg := process_command(full_command)
+		fmt.Println(command_keyword + "exit")
+		switch command_keyword {
 		case "exit":
 			os.Exit(0)
 		case "echo":
@@ -35,10 +37,18 @@ func main() {
 			if slices.Contains(builtin, arg) {
 				fmt.Println(arg + " is a shell builtin")
 			} else {
+				dirs := strings.Split(PATH, string(filepath.ListSeparator))
+				for i := 0; i < len(dirs); i++ {
+					if _, err := os.Stat(dirs[i] + arg); err == nil {
+						fmt.Println("valid_command is " + dirs[i])
+						break
+					}
+				}
 				fmt.Println(arg + ": not found")
 			}
+
 		default:
-			fmt.Println(command[:len(command)-1] + ": command not found")
+			fmt.Println(full_command[:len(full_command)-1] + ": command not found")
 		}
 
 		if err != nil {
