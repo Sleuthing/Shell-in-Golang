8d813c92ee7e9493aac6b6736008a43b50138acd
Sleuthing <anaslaykah@yahoo.com>
2025-02-26 08:59:33 +0100

    redirect-stdout-test-rerun

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -20,16 +20,18 @@ var original_stdout = os.Stdout
 // print functions
 
 // print output if no error exists otherwise print error
-func print_output_if_err_nil(output string, err error) {
+func get_output_or_err_message(output string, err error) string {
 	if err == nil {
-		fmt.Print(string(output))
+		return string(output)
+		//fmt.Print(string(output))
 	} else {
-		fmt.Fprintln(os.Stderr, "Error executing input:", err)
+		return fmt.Sprintf("Error executing input: %s", err)
+		//fmt.Fprintln(os.Stderr, "Error executing input:", err)
 	}
 }
 
-func print_no_such_file_or_directory(cmd_keywrd string, dir string) {
-	fmt.Println(cmd_keywrd + ": " + dir + ": " + "No such file or directory")
+func get_no_such_file_or_directory_message(cmd_keywrd string, dir string) string {
+	return cmd_keywrd + ": " + dir + ": " + "No such file or directory"
 }
 
 // boolean functions
@@ -82,14 +84,23 @@ func check_for_stdout_redir(arg string) (string, string) {
 	return arg, ""
 }
 
+// func get_output_file(output_path string) (*os.File, []byte)
 func get_output_file(output_path string) *os.File {
+	// var old_content []byte
+	// fmt.Println("length of output_path: " + string(len(output_path)))
 	if output_path != "" {
+		// search_result := search_executable_path(output_path)
+		// fmt.Println(search_result)
+		// if search_result != "" {
+		// old_content, _ = os.ReadFile(search_result)
+		// fmt.Println(old_content)
+		// }
 		outfile, err := os.Create(output_path)
 
 		if err != nil {
 			panic(err)
 		}
-
+		// return outfile, old_content
 		return outfile
 	}
 	return nil
@@ -109,62 +120,80 @@ func all[T any](slice []T, fn func(T) bool) (bool, *T) {
 func main() {
 	for i := 0; i < 100; i++ {
 		fmt.Fprint(os.Stdout, "$ ")
+		var output_string string
 		full_command, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		clean_command := clean_command_clause(full_command)
 		command_sentence, output_path := check_for_stdout_redir(clean_command)
 		command_keyword, arg_clause := process_command(command_sentence)
+		// var outfile, original_file_content = get_output_file(output_path)
 		var outfile = get_output_file(output_path)
 		if outfile != nil {
+			// fmt.Println(original_file_content)
 			os.Stdout = outfile
 			defer outfile.Close()
 		}
 		switch command_keyword {
 		case "exit":
+			//fmt.Println("logout")
 			os.Exit(0)
 		case "echo":
-			fmt.Println(strings.Trim(arg_clause, "'"))
+			output_string = strings.Trim(arg_clause, "\"'")
+			//fmt.Println(strings.Trim(arg_clause, "\"'"))
 		case "pwd":
 			directory, err := os.Getwd()
-			print_output_if_err_nil(directory, err)
-			fmt.Println()
+			output_string = get_output_or_err_message(directory, err)
+			// fmt.Println()
 		case "cd":
 			if arg_clause == "~" {
 				os.Chdir(HOME)
 			} else if path_is_valid(arg_clause) {
 				os.Chdir(arg_clause)
 			} else {
-				print_no_such_file_or_directory(command_keyword, arg_clause)
+				output_string = get_no_such_file_or_directory_message(command_keyword, arg_clause)
 			}
 		case "type":
 			if is_builtin(arg_clause) {
-				fmt.Println(arg_clause + " is a shell builtin")
+				// fmt.Println(arg_clause + " is a shell builtin")
+				output_string = arg_clause + " is a shell builtin"
 			} else {
 				search_result := search_executable_path(arg_clause)
 				if search_result == "" {
-					fmt.Println(arg_clause + ": not found")
+					// fmt.Println(arg_clause + ": not found")
+					output_string = arg_clause + ": not found"
 				} else {
-					fmt.Println(arg_clause + " is " + search_result)
+					// fmt.Println(arg_clause + " is " + search_result)
+					output_string = arg_clause + " is " + search_result
 				}
 			}
 		default:
 			search_result := search_executable_path(command_keyword)
 			if search_result == "" {
-				fmt.Println(clean_command + ": command not found")
+				output_string = clean_command + ": command not found"
+				// fmt.Println(clean_command + ": command not found")
 			} else {
 				args := strings.Split(arg_clause, " ")
 				safe_to_execute := true
-				if command_keyword == "cat" {
-					all_path_args_valid, invalid_path := all(args, path_is_valid)
-					if !all_path_args_valid && invalid_path != nil {
-						os.Stdout = original_stdout
-						safe_to_execute = false
-						print_no_such_file_or_directory(command_keyword, *invalid_path)
-					}
-				}
+				// if command_keyword == "cat" {
+				// 	for i=0;i<len(args);i++{
+				// 		command_result := exec.Command(command_keyword, args...)
+				// 		output, err := command_result.Output()
+				// 		output_string+=string(output)+"\n"
+
+				// 	}
+				// 	all_path_args_valid, invalid_path := all(args, path_is_valid)
+				// 	if !all_path_args_valid && invalid_path != nil {
+				// 		os.Stdout = original_stdout
+				// 		// outfile.Write(original_file_content)
+				// 		// fmt.Println("something Happened")
+				// 		safe_to_execute = false
+				// 		output_string = get_no_such_file_or_directory_message(command_keyword, *invalid_path)
+				// 	}
+				// }
 				if safe_to_execute {
+					// fmt.Println("nothing Happened")
 					command_result := exec.Command(command_keyword, args...)
 					output, err := command_result.Output()
-					print_output_if_err_nil(string(output), err)
+					output_string = get_output_or_err_message(string(output), err)
 				}
 
 			}
@@ -176,6 +205,7 @@ func main() {
 			fmt.Fprintln(os.Stderr, "Error reading input:", err)
 			os.Exit(1)
 		}
+		fmt.Println(output_string)
 		os.Stdout = original_stdout
 	}
 }
